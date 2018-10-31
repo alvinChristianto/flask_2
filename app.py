bd89df292c25491a7e3d54f299543f5bd954a225
alvinVMWare <alvinchristianto93@gmail.com>
2018-10-31 22:37:39 +0700

    eighth

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def index():
     return jsonify(Products=scrape())
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
